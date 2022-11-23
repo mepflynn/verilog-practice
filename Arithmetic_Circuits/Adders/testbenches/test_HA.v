eb14b5d010c943837dcb8a4d2775d15b6e008f1b
MAX FLYNN <49320957+mepflynn@users.noreply.github.com>
2022-11-23 00:07:13 -0800

    Rewrote HA TB using Assertions

--- Arithmetic_Circuits/Adders/testbenches/test_HA.v
+++ Arithmetic_Circuits/Adders/testbenches/test_HA.v
@@ -27,3 +27,33 @@ module test_HA;
         end
     end
 endmodule
+
+
+module test_HA_Assertion;
+    // Declare I/O Signals for DUT
+    // Combine the two inputs for ease of cycling through cases
+    reg [1:0] ab;
+    
+    wire s;
+    wire c;
+
+    // Instantiate DUT
+    HA h0(ab[1],ab[0],s,c);
+
+    // Excite through all states, check against software addition on signal ref
+  	reg [1:0] x;
+    initial begin
+        ab = 2'd0;
+        x = 2'd0;
+
+        repeat(4) begin
+            #100
+            // Note that we add the bits of ref separately, as they are both weighted 1 (the 0th order bit)
+          SUMeqX: assert ({c,s} == (x[1] + x[0])) $display("OK. Sum = reference.");
+          	else $error("Error: Output Mismatch");
+
+            ab = ab + 1;
+            x = x + 1;
+        end
+    end
+endmodule
