924c299c227baef70b7b2e87bfe6f812784ea030
MAX FLYNN <49320957+mepflynn@users.noreply.github.com>
2022-12-01 23:50:40 -0800

    Fixed FA testbench issue - using ref keyword as reg name

--- Arithmetic_Circuits/Adders/testbenches/test_FA.v
+++ Arithmetic_Circuits/Adders/testbenches/test_FA.v
@@ -11,23 +11,23 @@ module test_FA;
     // Instantiate DUT
     FA f0(in[2],in[1],in[0],s,co);
 
-    // Excite through all states, check against software addition on signal ref
-    reg [2:0] ref;
+    // Excite through all states, check against software addition on signal ref_sum
+    reg [2:0] ref_sum;
     initial begin
         in = 3'd0;
-        ref = 3'd0;
+        ref_sum = 3'd0;
 
         repeat(8) begin
             #100
-            // Note that we add the bits of ref separately, as they are both weighted 1 (the 0th order bit)
-            if ({1'b0,co,s} !== (ref[2] + ref[1] + ref[0])) begin
+            // Note that we add the bits of ref_sum separately, as they are both weighted 1 (the 0th order bit)
+            if ({1'b0,co,s} !== (ref_sum[2] + ref_sum[1] + ref_sum[0])) begin
                 // Error detected!
-                $display("Error: Output = %b, reference = %b",{co,s},ref);
+                $display("Error: Output = %b, ref_sum = %b",{co,s},ref_sum);
             end else begin 
-                $display("In: %b | Out: %b | Ref: %b | Sum: %b",in,{1'b0,co,s},ref);
+                $display("In: %b | Out: %b | ref_sum: %b | Sum: %b",in,{1'b0,co,s},ref_sum);
             end
             in = in + 1;
-            ref = ref + 1;
+            ref_sum = ref_sum + 1;
         end
     end
 endmodule
